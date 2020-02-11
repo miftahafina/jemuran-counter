0a86062e065a783442de932c46082d03af70ab62
Miftah Afina <miftah.afina@gmail.com>
2020-02-11 16:27:04 +0700

    Remove reload button

--- src/containers/pages/About/About.jsx
+++ src/containers/pages/About/About.jsx
@@ -2,29 +2,11 @@ import React, { Component } from 'react';
 import Header from '../../../components/Header/Header';
 
 class About extends Component {
-  handleReload = () => {
-    ServiceWorkerRegistration.onupdatefound = () => {
-      // Check if online
-      if (navigator.onLine) {
-        // Delete cache
-        caches.keys()
-          .then(function(names) {
-            for (let name of names)
-                caches.delete(name);
-          })
-
-          .then(() => {
-            window.location.reload(true);
-          })
-      }
-    }
-  }
-
   render() {
     return (
       <div className="container">
         <Header title="About"/>
-        <button onClick={this.handleReload}>Reload...</button>
+        <h1>Update 1</h1>
       </div>
     );
   }
