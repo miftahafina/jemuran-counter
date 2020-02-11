a82ec0feb2a722a866e801ab4671b50d69d616ab
Miftah Afina <miftah.afina@gmail.com>
2020-02-11 16:03:12 +0700

    Add reload button on about page

--- src/containers/pages/About/About.jsx
+++ src/containers/pages/About/About.jsx
@@ -2,10 +2,29 @@ import React, { Component } from 'react';
 import Header from '../../../components/Header/Header';
 
 class About extends Component {
+  handleReload = () => {
+    ServiceWorkerRegistration.onupdatefound = () => {
+      // Check if online
+      if (navigator.onLine) {
+        // Delete cache
+        caches.keys()
+          .then(function(names) {
+            for (let name of names)
+                caches.delete(name);
+          })
+
+          .then(() => {
+            window.location.reload(true);
+          })
+      }
+    }
+  }
+
   render() {
     return (
       <div className="container">
-        <Header title="About..."/>
+        <Header title="About...."/>
+        <button onClick={this.handleReload}>Reload</button>
       </div>
     );
   }
