d97123795b018b6ba85c66ec03d0f432b442318d
Miftah Afina <miftah.afina@gmail.com>
2020-02-11 15:07:39 +0700

    Revert about page

--- src/containers/pages/About/About.jsx
+++ src/containers/pages/About/About.jsx
@@ -5,7 +5,7 @@ class About extends Component {
   render() {
     return (
       <div className="container">
-        <Header title="About Me"/>
+        <Header title="About"/>
       </div>
     );
   }
