25d56a0fdb4308a101114e58e7c603e4ebf30374
Miftah Afina <miftah.afina@gmail.com>
2020-02-12 17:36:50 +0700

    Use props.history.push by wrapping NavBar component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,26 +1,37 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+// Libraries
+import React, { Component } from 'react';
+import { withRouter } from 'react-router';
+
+// Static
 import calculator from '../../img/calculator.svg';
 import hourglass from '../../img/hourglass.svg';
 import resume from '../../img/resume.svg';
 
-const Footer = () => {
-  return (
-    <nav className="navbar">
-      <Link to="/" className="active">
-        <img src={calculator} alt="icon calculator"/>
-        <p>Counter</p>
-      </Link>
-      <Link to="/timer">
-        <img src={hourglass} alt="icon timer"/>
-        <p>Timer</p>
-      </Link>
-      <Link to="/about">
-        <img src={resume} alt="icon resume"/>
-        <p>About</p>
-      </Link>
-    </nav>
-  );
+class Navbar extends Component {
+  handleNav = (destination) => {
+    this.props.history.push(destination);
+  }
+
+  render() {
+    return (
+      <nav className="navbar">
+        <button className="active" onClick={() => {this.handleNav('/')}}>
+          <img src={calculator} alt="calculator"/>
+          <p>Counter</p>
+        </button>
+
+        <button className="" onClick={() => {this.handleNav('/timer')}}>
+          <img src={hourglass} alt="timer"/>
+          <p>Timer</p>
+        </button>
+        
+        <button className="" onClick={() => {this.handleNav('/about')}}>
+          <img src={resume} alt="resume"/>
+          <p>About</p>
+        </button>
+      </nav>
+    );
+  }
 }
 
-export default Footer;
+export default withRouter(Navbar);
