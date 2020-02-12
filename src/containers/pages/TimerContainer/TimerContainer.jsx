6d2ad184698b4e5a264dc5c07a762b67971ff0ca
Miftah Afina <miftah.afina@gmail.com>
2020-02-12 22:30:41 +0700

    Add current timer name state

--- src/containers/pages/TimerContainer/TimerContainer.jsx
+++ src/containers/pages/TimerContainer/TimerContainer.jsx
@@ -2,6 +2,28 @@ import React, { Component } from 'react';
 import Header from '../../../components/Header/Header';
 
 class TimerContainer extends Component {
+  state = {
+    timeLeft: '00:00',
+    currentTimerName: ''
+  }
+
+  handleSetTimer = (timerName, duration) => {
+    this.setState({
+      currentTimerName: timerName,
+      timeLeft: duration
+
+    }, () => {
+      console.log(this.state.currentTimerName);
+
+      setTimeout(() => {
+        this.setState({
+          timeLeft: this.state.timeLeft - 1
+        })
+      }, 1000);
+
+    });
+  }
+
   render() {
     return (
       <div className="container">
@@ -9,28 +31,28 @@ class TimerContainer extends Component {
 
         <div className="section display-counter">
           <div className="circle-sm">
-            <p className="clock-timer">25:34</p>
-            <small className="unit">Cuci + Rendam</small>
+            <p className="clock-timer">{this.state.timeLeft}</p>
+            <small className="unit">{this.state.currentTimerName}</small>
           </div>
         </div>
 
         <div className="section btn-timer">
           <div className="btn btn-md ml-8">
-            <button className="current-timer">
+            <button onClick={() => this.handleSetTimer('Cuci + Rendam', 45)} className={this.state.currentTimerName === 'Cuci + Rendam' ? 'current-timer' : ''}>
               <p>Cuci + Rendam</p>
               <small>45<br />menit</small>
             </button>
           </div>
 
           <div className="btn btn-md ml-8">
-            <button>
+            <button onClick={() => this.handleSetTimer('Bilas', 15)} className={this.state.currentTimerName === 'Bilas' ? 'current-timer' : ''}>
               <p>Bilas</p>
               <small>15<br />menit</small>
             </button>
           </div>
 
           <div className="btn btn-md ml-8">
-            <button>
+            <button onClick={() => this.handleSetTimer('Pengeringan', 5)} className={this.state.currentTimerName === 'Pengeringan' ? 'current-timer' : ''}>
               <p>Pengeringan</p>
               <small>5<br />menit</small>
             </button>
