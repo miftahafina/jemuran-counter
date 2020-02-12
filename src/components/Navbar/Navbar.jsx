849f850603122fe07a9f59ed9751e3fbe39f7d52
Miftah Afina <miftah.afina@gmail.com>
2020-02-12 18:04:06 +0700

    Add active status on navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,24 +8,91 @@ import hourglass from '../../img/hourglass.svg';
 import resume from '../../img/resume.svg';
 
 class Navbar extends Component {
-  handleNav = (destination) => {
-    this.props.history.push(destination);
+  state = {
+    location: this.props.history.location,
+    nav: {
+      counter: '',
+      timer: '',
+      about: ''
+    }
   }
 
+  componentDidMount() {
+    const currentPage = this.props.history.location.pathname;
+    this.onNavChange(currentPage);
+  }
+
+  handleNav = (destination) => {
+    this.props.history.push(destination);
+    this.setState({
+      location: this.props.history.location
+    }, () => {
+      this.onNavChange(destination);
+    });
+  }
+
+  onNavChange = (currentPage) => {
+    switch (currentPage) {
+      case '/':
+        this.setState({
+          nav: {
+            counter: 'active',
+            timer: '',
+            about: ''
+          }
+        });
+        break;
+
+      case '/timer':
+        this.setState({
+          nav: {
+            counter: '',
+            timer: 'active',
+            about: ''
+          }
+        });
+        break;
+
+      case '/about':
+        this.setState({
+          nav: {
+            counter: '',
+            timer: '',
+            about: 'active'
+          }
+        });
+        break;
+
+      default:
+        this.setState({
+          nav: {
+            counter: 'active',
+            timer: '',
+            about: ''
+          }
+        });
+        break;
+    }
+  }
+  
+
   render() {
+    let { counter, timer, about } = this.state.nav;
+    let { handleNav } = this;
+
     return (
       <nav className="navbar">
-        <button className="active" onClick={() => {this.handleNav('/')}}>
+        <button className={counter} onClick={() => {handleNav('/')}}>
           <img src={calculator} alt="calculator"/>
           <p>Counter</p>
         </button>
 
-        <button className="" onClick={() => {this.handleNav('/timer')}}>
+        <button className={timer} onClick={() => {handleNav('/timer')}}>
           <img src={hourglass} alt="timer"/>
           <p>Timer</p>
         </button>
         
-        <button className="" onClick={() => {this.handleNav('/about')}}>
+        <button className={about} onClick={() => {handleNav('/about')}}>
           <img src={resume} alt="resume"/>
           <p>About</p>
         </button>
